17ceb6ee63b105245db9ec531b523e9b4945186c
morathias <matias_morales_1996@hotmail.com>
2016-09-15 13:02:54 -0300

    Import Meshes
    se implemento assimp.
    falta importar las meshes

--- Engine - copia/EngineLib/Importer.cpp
+++ Engine - copia/EngineLib/Importer.cpp
@@ -1,10 +1,17 @@
 #include "Importer.h"
-
-Importer::Importer()
-{
+#include "assimp\Importer.hpp"
+#include "assimp\scene.h"
+#include "assimp\postprocess.h"
+//================================================
+Importer::Importer(){
 }
-
-
-Importer::~Importer()
-{
+//================================================
+Importer::~Importer(){
+}
+//================================================
+bool Importer::importMesh(const std::string& fileName, Mesh& mesh){
+	Assimp::Importer importer;
+	const aiScene* scene = importer.ReadFile(fileName, aiProcess_CalcTangentSpace |
+													   aiProcess_Triangulate);
+	return true;
 }
